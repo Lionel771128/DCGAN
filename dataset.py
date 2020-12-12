cb32204bd74e97bc8763f35c936039da4c68802d
lionel771128 <lionel771128@gmail.com>
2020-12-12 09:23:35 +0800

    dataset edit

--- dataset.py
+++ dataset.py
@@ -7,25 +7,38 @@ import numpy as np
 
 
 class FaceDataset(Dataset):
-    def __init__(self, datapath, image_size=128, augment=False):
+    def __init__(self, datapath, image_size=128, augment=False, cache_image=False):
         self.augment = augment
         self.image_size = image_size
+        self.cache_image = cache_image
         self.trms = transforms.Compose([transforms.ToTensor(),
                                         transforms.Normalize(mean=[0.5, 0.5, 0.5],
                                                              std=[0.5, 0.5, 0.5])])
-
+        # mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
         with open(datapath, 'r') as f:
             self.image_path_list = f.readlines()
+
+        if cache_image:
+            self.image_list = []
+            for img_path in self.image_path_list:
+                self.image_list.append(cv2.imread(img_path.strip()))
 
     def __cv2ToPillow(self, image):
         assert type(image) == np.ndarray, 'input should be a ndarray!'
         return Image.fromarray(image)
 
     def __getitem__(self, idx):
-        image = cv2.imread(self.image_path_list[idx])
+        if self.cache_image:
+            image = self.image_list[idx]
+        else:
+            image = cv2.imread(self.image_path_list[idx].strip())
         # assert image.shape[:1] == (self.image_size, self.image_size), f'image size should be {self.image_size}'
         if self.augment:
+            center = (image.shape[0] // 2, image.shape[1] // 2)
+            dh, dw = self.image_size // 2, self.image_size // 2
+            image = image[center[0] - dh: center[0] + dh, center[1] - dw: center[1] + dw, :]
             pass
+        image = cv2.resize(image, (self.image_size, self.image_size))
         image = self.trms(image)
         label = torch.ones(1)
         return image, label
--- train.py
+++ train.py
@@ -1,15 +1,21 @@
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
-from models import Discriminator, Generator
-from dataset import FaceDataset
 from torch.optim import SGD, lr_scheduler
+import torch.cuda.amp as amp
 from tqdm import tqdm
 import math
+
+from models import Discriminator, Generator
+from dataset import FaceDataset
+
+
 config = {
     'image_size': 128,
-    'batch_size': 128,
+    'batch_size': 100,
     'ngf': 512,
     'ndf': 16,
+    'nzf': 100,
     'optimizer': 'sgd',
     'lr': 0.01,
     'momentum': 0.9,
@@ -17,17 +23,19 @@ config = {
     'lr_schedule': None,
     'epochs': 100
     }
+image_size = config['image_size']
+batch_size = config['batch_size']
 
-data_path = '/Users/lionl771128/Documents/DL_Project/GAN/DCGAN/train_128.txt'
-train_set = FaceDataset(data_path, 128)
+data_path = '/home/scott/Desktop/dataset/face3k/train.txt'
+train_set = FaceDataset(data_path, 128, augment=True, cache_image=False)
 train_loader = DataLoader(train_set,
-                          batch_size=config['batch_size'],
+                          batch_size=batch_size,
                           shuffle=True,
                           num_workers=8,
                           pin_memory=True)
 
-netG = Generator(input_size=(config['batch_size'], 100, 1, 1),
-                 image_size=config['image_size'],
+netG = Generator(input_size=(batch_size, 100, 1, 1),
+                 image_size=image_size,
                  ngf=config['ngf'],
                  leaky_relu=True)
 netD = Discriminator(image_size=config['image_size'],
@@ -39,6 +47,7 @@ load trained weight
 
 '''
 create optimizer & lr schedule
+TTL
 '''
 # filter 使用方式
 optimizerD = None
@@ -47,17 +56,65 @@ if config['optimizer'] == 'sgd':
     optimizerD = SGD(params=filter(lambda p: p.requires_grad, netD.parameters()),
                      lr=config['lr'],
                      momentum=config['momentum'],
-                     nesterov=config['nesterov'])
+                     nesterov=config['nestrov'])
     optimizerG = SGD(params=filter(lambda p: p.requires_grad, netG.parameters()),
                      lr=config['lr'],
                      momentum=config['momentum'],
-                     nesterov=config['nesterov'])
+                     nesterov=config['nestrov'])
 
 
 epochs = config['epochs']
-lr_scheduler = None
+lr_sch = None
 if config['lr_schedule'] == 'exp-1.5pi':
     lr_lambda = lambda x: math.exp(-1.5 * x * math.pi / epochs)
-    lr_scheduler.LambdaLR(optimizerD, lr_lambda=lr_lambda, last_epoch=epochs)
+    lr_sch = lr_scheduler.LambdaLR(optimizerD, lr_lambda=lr_lambda, last_epoch=epochs)
 
+
+'''
+create loss
+'''
+# criterion = nn.CrossEntropyLoss()
+criterion = nn.BCEWithLogitsLoss()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+netD.to(device)
+netG.to(device)
+for epoch in range(epochs):
+    pbar = tqdm(train_loader, leave=True, total=len(train_loader))
+    for b_idx, (image, target) in enumerate(pbar):
+        optimizerG.zero_grad()
+        optimizerD.zero_grad()
+
+        netD.train()
+        netG.train()
+        '''
+        train Discriminator
+        1. train D with real data & real labels
+        2. train G with fake data which is created by G and noise z
+         
+        '''
+
+        out = netD(image.to(device)).view(-1)
+
+        real_targets = torch.ones(image.shape[0]).to(device)
+
+        loss_D_real = criterion(out, real_targets)
+        loss_D_real.backward()
+
+        z = torch.randn([config['batch_size'], config['nzf'], 1, 1], device=device)
+        fake_data = netG(z)
+        fake_targets = torch.zeros(fake_data.shape[0]).to(device)
+        out = netD(fake_data.detach()).view(-1)
+        loss_D_fake = criterion(out, fake_targets)
+        loss_D_fake.backward()
+        optimizerD.step()
+
+
+        fake_data = torch.randn([batch_size, 100, 1, 1]).to(device)
+
+
+        out_G = netG(fake_data)
+        fake_target = torch.ones(out_G.shape[0]).to(device)
+        loss_G = -1 * criterion(netD(out_G).view(-1), fake_target)
+        loss_G.backward()
+        optimizerG.step()
 
